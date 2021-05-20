456e98e55668c078950924ba3e6703cde9f9e8d6
Carlos <53526597+carlosgauci@users.noreply.github.com>
2021-05-20 14:29:36 +0200

    add api call

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,31 @@
-<template> </template>
+<template>
+  <h1>Home</h1>
+</template>
 
 <script>
 export default {
   name: "Home",
+
+  data() {
+    return {
+      weatherData: {},
+    };
+  },
+
+  methods: {
+    // Fetch weather data from the api
+    async fetchWeather() {
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=35.8997&lon=14.5147&units=metric&exclude=minutely,hourly&appid=${process.env.VUE_APP_APIKEY}`
+      );
+      const data = await res.json();
+      this.weatherData = data;
+      console.log(this.weatherData);
+    },
+  },
+
+  async created() {
+    this.weatherData = await this.fetchWeather();
+  },
 };
 </script>
